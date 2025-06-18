4d5ef55776759064d3f4ba815e2bb7fb0aab1093
Vladyslav Vozniuk <voznjuckv@gmail.com>
2025-06-18 18:32:21 +0300

    feat(DART-06): task 7 – switch statement for weekday

--- 1_task.dart
+++ 1_task.dart
@@ -98,6 +98,36 @@ void main() {
       print('Повторити курс');
     }
   }
-  
+  // Завдання 7: Конструкція switch statement
+
+  int dayNumber = 6;
+
+  switch (dayNumber) {
+    case 1:
+      print('Понеділок');
+      break;
+    case 2:
+      print('Вівторок');
+      break;
+    case 3:
+      print('Середа');
+      break;
+    case 4:
+      print('Четвер');
+      break;
+    case 5:
+      print('Пʼятниця');
+      break;
+    case 6:
+      print('Субота');
+      print('Вихідний день 🎉');
+      break;
+    case 7:
+      print('Неділя');
+      print('Вихідний день 🎉');
+      break;
+    default:
+      print('Невірний номер дня');
+  }
 }
 
