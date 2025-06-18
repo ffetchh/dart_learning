4e6b1597928016e6731264bbbb04479d957f57f8
Vladyslav Vozniuk <voznjuckv@gmail.com>
2025-06-18 18:31:07 +0300

    feat(DART-06): task 6 – if/else grading system

--- 1_task.dart
+++ 1_task.dart
@@ -79,6 +79,25 @@ void main() {
 
   // Вивід результату
   print('Остаточний результат: $finalScore');
+
+  // Завдання 6: Умовні конструкції if/else
+
+  int examScore = 56;
+
+  if (examScore > 90) {
+    print('Відмінно');
+  } else if (examScore >= 75) {
+    print('Добре');
+  } else if (examScore >= 60) {
+    print('Задовільно');
+  } else {
+    print('Не здано');
+
+    // Додаткова перевірка
+    if (examScore < 20) {
+      print('Повторити курс');
+    }
+  }
   
 }
 
